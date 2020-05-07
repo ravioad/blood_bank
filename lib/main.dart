23759ca6cd448e211d7a4b61b70f4f269ed86232
ravioad <63903568+ravioad@users.noreply.github.com>
2020-05-07 15:32:10 +0500

    improved startup ui1

--- lib/main.dart
+++ lib/main.dart
@@ -29,103 +29,97 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage>
     with SingleTickerProviderStateMixin {
   bool _isLogin = true;
+  bool _clicked = false;
   Color textColor = Colors.grey[600];
-  AnimationController _scaleController;
-  Animation<double> _scaleAnimation;
-  @override
-  void initState() {
-    _scaleController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 800));
-    _scaleAnimation = Tween<double>(begin: 1, end: 30).animate(_scaleController)
-      ..addStatusListener((status) {
-        if(status==AnimationStatus.completed){
-         Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: _isLogin?LoginPage():SignupPage())); 
-          _scaleController.reverse();
-        }
-      });
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: Container(
-            color: Color(0xFFF3D4DA),
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/background.jpg'),
-                  fit: BoxFit.fitWidth),
+        body: SafeArea(
+          top: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.only(top: 30, left: 20),
+              color: Color(0xFFF3D4DA),
+              child: Text("Blood Bank", style: TextStyle(color: Colors.grey[500], fontSize: 45, fontWeight: FontWeight.bold),),
             ),
           ),
-        ),
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Color(0xFFD1CFDA),
-            child: Padding(
-              padding: EdgeInsets.all(30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    _isLogin ? "Login" : "Sign Up", //Color(0xFF212121)
-                    style: TextStyle(
-                        color: textColor,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  InkWell(
-                    child: AnimatedBuilder(
-                      animation: _scaleAnimation,
-                      builder: (context, child) => Transform.scale(
-                        scale: _scaleAnimation.value,
-                        child: Container(
-                          padding: EdgeInsets.all(35),
-                          decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Color(0xFFeab6c0),
-                              image: DecorationImage(
-                                image: AssetImage(_isLogin
-                                    ? 'assets/images/logout32.png'
-                                    : 'assets/images/login32.png'),
-                              )),
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                      _scaleController.forward();
-                    },
-                  ),
-                  FlatButton(
-                    child: Text(
-                      _isLogin
-                          ? "Don't have account? Signup here:"
-                          : "Already have account? Login here:",
-                      style: TextStyle(color: textColor, fontSize: 15),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        _isLogin = !_isLogin;
-                      });
-                    },
-                  )
-                ],
+          Expanded(
+            flex: 3,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/background.jpg'),
+                    fit: BoxFit.fitWidth),
               ),
             ),
           ),
-        )
-      ],
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Color(0xFFD1CFDA),
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 40,
+                    ),
+                    InkWell(
+                      child: Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          gradient: LinearGradient(colors: [
+                            Color(0xFFed97a7),
+                            Color(0xFFeab6c0),
+                          ]),
+                        ),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(
+                            _isLogin ? "Login" : "Sign Up", //Color(0xFF212121)
+                            style: TextStyle(
+                                color: Colors.white70,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.fade,
+                                child: _isLogin ? LoginPage() : SignupPage()));
+                      },
+                    ),
+                    FlatButton(
+                      child: Text(
+                        _isLogin
+                            ? "Don't have account? Signup here:"
+                            : "Already have account? Login here:",
+                        style: TextStyle(color: textColor, fontSize: 15),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
